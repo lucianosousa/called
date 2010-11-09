cdf689e332cb4bc0c84388ae3b12606d599dfeac
Luciano Sousa <ls@lucianosousa.net>
2010-11-08 23:13:38 -0200

    added all tests for create user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,5 +13,22 @@ describe User do
     user.should_not be_valid
   end
 
+  it 'should not create user without confirmation password' do
+    user = User.create(:login => 'login name', :password => 'password')
+    user.save
+    user.should_not be_valid
+  end
 
+  it 'should not create user without email' do
+    user = User.create(:login => 'login name', :password => 'password', :password_confirmation => 'password')
+    user.save
+    user.should_not be_valid
+  end
+
+  it 'should create user with all requirements' do
+    user = User.create(:login => 'login name', :email => 'ls@lucianosousa.net', :password => 'password', :password_confirmation => 'password')
+    user.save
+    user.should be_valid
+  end
+  
 end
